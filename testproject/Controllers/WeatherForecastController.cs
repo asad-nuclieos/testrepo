89a7cf410b01c907857703037013abafad7d1543
Lenovo <Lenovo@DESKTOP-8F98NSH>
2024-08-02 11:15:32 +0500

    push latest code

--- testproject/Controllers/WeatherForecastController.cs
+++ testproject/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace testproject.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecasttest")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
